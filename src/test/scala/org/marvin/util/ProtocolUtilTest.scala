5f62222bb0434e832791c9349192448b15d5b434
daniel.takabayashi <daniel.takabayashi@gmail.com>
2017-10-03 14:22:17 -0300

    fix package name

--- src/test/scala/org/marvin/util/ProtocolUtilTest.scala
+++ src/test/scala/org/marvin/util/ProtocolUtilTest.scala
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
   */
-package org.marvin.util.service
+package org.marvin.util
 
 import org.marvin.util.ProtocolUtil
 import org.scalatest.{Matchers, WordSpec}
