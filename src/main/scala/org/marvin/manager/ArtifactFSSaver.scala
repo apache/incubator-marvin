cafb4c1cadb82ae1cab779fef6c18d04935cf6d5
Daniel Martins Takabayashi <daniel.takabayashi@gmail.com>
2017-11-30 15:23:10 -0200

    Added ArtifactFSSaver class to close #27 (#28)
    * Added ArtifactFSSaver class to close #27

---
+++ src/main/scala/org/marvin/manager/ArtifactFSSaver.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright [2017] [B2W Digital]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.marvin.manager
+
+import java.nio.file.{Files, Path, Paths, StandardCopyOption}
+
+import akka.Done
+import akka.actor.{Actor, ActorLogging}
+import org.marvin.manager.ArtifactSaver.{SaveToLocal, SaveToRemote}
+import org.marvin.model.EngineMetadata
+
+class ArtifactFSSaver(metadata: EngineMetadata) extends Actor with ActorLogging {
+  override def preStart() = {
+    log.info(s"${this.getClass().getCanonicalName} actor initialized...")
+  }
+
+  def generatePaths(artifactName: String, protocol: String): Map[String, Path] = {
+    Map(
+      "localPath" -> Paths.get(s"${metadata.artifactsLocalPath}/${metadata.name}/$artifactName"),
+      "remotePath" -> Paths.get((s"${metadata.artifactsRemotePath}/${metadata.name}/${metadata.version}/$artifactName/$protocol"))
+    )
+  }
+
+  def copyFile(origin: Path, destination: Path): Unit = {
+    if (!destination.getParent.toFile.exists()) destination.getParent.toFile.mkdirs()
+
+    log.info(s"Copying files from ${origin} to ${destination}")
+
+    Files.copy(origin, destination, StandardCopyOption.REPLACE_EXISTING)
+
+    log.info(s"File ${destination} saved!")
+  }
+
+  override def receive: Receive = {
+    case SaveToLocal(artifactName, protocol) =>
+      log.info("Receive message and starting to working...")
+      val uris = generatePaths(artifactName, protocol)
+
+      copyFile(uris("remotePath"), uris("localPath"))
+
+      sender ! Done
+
+    case SaveToRemote(artifactName, protocol) =>
+      log.info("Receive message and starting to working...")
+      val uris = generatePaths(artifactName, protocol)
+
+      copyFile(uris("localPath"), uris("remotePath"))
+
+      sender ! Done
+
+    case _ =>
+      log.warning("Received a bad format message...")
+  }
+}
--- src/test/scala/org/marvin/executor/manager/ArtifactSaverTest.scala
+++ src/test/scala/org/marvin/executor/manager/ArtifactSaverTest.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright [2017] [B2W Digital]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.marvin.executor.manager
 
 import org.marvin.manager.ArtifactSaver
@@ -14,9 +30,15 @@ class ArtifactSaverTest extends WordSpec with Matchers {
     "version", "engineType", null, "artifactsRemotePath", "S3", "marvin-artifact-bucket", null,
     3000, 3000, 3000, Option(3000), 3000, "testHost")
 
+  val engineMetadata3: EngineMetadata = new EngineMetadata("name",
+    "version", "engineType", null, "artifactsRemotePath", "fs", null, null,
+    3000, 3000, 3000, Option(3000), 3000, "testHost")
+
   val props1 = ArtifactSaver.build(engineMetadata1)
 
   val props2 = ArtifactSaver.build(engineMetadata2)
+
+  val props3 = ArtifactSaver.build(engineMetadata3)
 
   "A engineMetadata with artifactsSaverType as HDFS" should {
     "return Props with actorClass ArtifactHdfsSaver" in {
@@ -27,6 +49,12 @@ class ArtifactSaverTest extends WordSpec with Matchers {
   "A engineMetadata with artifactsSaverType as S3" should {
     "return Props with actorClass ArtifactS3Saver" in {
       assert(props2.actorClass().toString == "class org.marvin.manager.ArtifactS3Saver")
+    }
+  }
+
+  "A engineMetadata with artifactsSaverType as FS" should {
+    "return Props with actorClass ArtifactFSSaver" in {
+      assert(props3.actorClass().toString == "class org.marvin.manager.ArtifactFSSaver")
     }
   }
 }
