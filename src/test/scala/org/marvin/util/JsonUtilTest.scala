daaea0de5a60fe2e976dec1b207b923d405fcda0
daniel.takabayashi <daniel.takabayashi@gmail.com>
2017-12-05 17:49:46 -0200

    remove ununsed imports

--- src/test/scala/org/marvin/util/JsonUtilTest.scala
+++ src/test/scala/org/marvin/util/JsonUtilTest.scala
@@ -17,13 +17,8 @@
 package org.marvin.util
 
 import java.io.File
-
 import org.everit.json.schema.ValidationException
 import org.marvin.model.EngineMetadata
-import java.io.File
-import org.everit.json.schema.ValidationException
-import org.json.JSONObject
-import org.marvin.model.{EngineMetadata, MarvinEExecutorException}
 import org.scalatest.{Matchers, WordSpec}
 import scala.io.Source
 
