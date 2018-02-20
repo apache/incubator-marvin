1c400c4823eff563e431a3f5e850bd6eeebfd61d
Yifei Zhang <yifei.z.luiz@gmail.com>
2018-02-20 08:55:04 -0300

    Update Acquisitor
    removing test code line

--- iris-species-engine/marvin_iris_species_engine/data_handler/acquisitor_and_cleaner.py
+++ iris-species-engine/marvin_iris_species_engine/data_handler/acquisitor_and_cleaner.py
@@ -29,5 +29,4 @@ class AcquisitorAndCleaner(EngineBaseDataHandler):
 
         iris = pd.read_csv(file_path)
         iris.drop('Id', axis=1, inplace=True)
-        raise Exception('Errorrrrrrrrrr!')
         self.initial_dataset = iris
