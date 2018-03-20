d60c5da2096f36dbfcbabc23cb6d61c6b841d474
zhang.yifei <zhang.yifei@b2wdigital.com>
2018-03-20 17:17:39 -0300

    span engine added

---
+++ sms-span-engine/marvin_sms_span_engine/prediction/prediction_preparator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""PredictionPreparator engine action.
+
+Use this module to add the project main code.
+"""
+
+from .._compatibility import six
+from .._logging import get_logger
+
+from marvin_python_toolbox.engine_base import EngineBasePrediction
+
+__all__ = ['PredictionPreparator']
+
+
+logger = get_logger('prediction_preparator')
+
+
+class PredictionPreparator(EngineBasePrediction):
+
+    def __init__(self, **kwargs):
+        super(PredictionPreparator, self).__init__(**kwargs)
+
+    def execute(self, input_message, params, **kwargs):
+        input_message = self.marvin_model["vect"].transform(input_message)
+
+        return input_message
