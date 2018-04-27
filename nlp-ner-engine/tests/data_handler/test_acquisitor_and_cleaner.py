6f53b1a2c7e9126dbab202cef320e4af413ef7ac
zhang.yifei <zhang.yifei@b2wdigital.com>
2018-04-27 14:30:30 -0300

    unit test fix

--- nlp-ner-engine/tests/data_handler/test_acquisitor_and_cleaner.py
+++ nlp-ner-engine/tests/data_handler/test_acquisitor_and_cleaner.py
@@ -13,8 +13,10 @@ from marvin_nlp_ner_engine.data_handler import AcquisitorAndCleaner
 
 
 @mock.patch('marvin_nlp_ner_engine.data_handler.acquisitor_and_cleaner.nltk.download')
-def test_execute(download_mocked, mocked_params):
+@mock.patch('marvin_nlp_ner_engine.data_handler.acquisitor_and_cleaner.nltk.corpus.conll2002.iob_sents')
+def test_execute(sents_mocked, download_mocked, mocked_params):
     ac = AcquisitorAndCleaner()
     ac.execute(params=mocked_params)
 
     download_mocked.assert_called_with('conll2002')
+    sents_mocked.assert_called_with('esp.testb')
