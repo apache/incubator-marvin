7ce79e3d423e949733251a95f2e89df72d4d237b
zhang.yifei <zhang.yifei@b2wdigital.com>
2018-04-27 15:10:01 -0300

    moeck function fix

--- nlp-ner-engine/tests/data_handler/test_acquisitor_and_cleaner.py
+++ nlp-ner-engine/tests/data_handler/test_acquisitor_and_cleaner.py
@@ -8,16 +8,15 @@ except ImportError:
     import unittest.mock as mock
 
 import nltk
-from mock import ANY
 from marvin_nlp_ner_engine.data_handler import AcquisitorAndCleaner
 
 
 @mock.patch('marvin_nlp_ner_engine.data_handler.acquisitor_and_cleaner.nltk.download')
-@mock.patch('marvin_nlp_ner_engine.data_handler.acquisitor_and_cleaner.nltk.corpus.conll2002.iob_sents')
-def test_execute(sents_mocked, download_mocked, mocked_params):
-    nltk.download('conll2002')
+@mock.patch('marvin_nlp_ner_engine.data_handler.acquisitor_and_cleaner.nltk.corpus')
+def test_execute(corpus_mocked, download_mocked, mocked_params):
+
     ac = AcquisitorAndCleaner()
     ac.execute(params=mocked_params)
 
     download_mocked.assert_called_with('conll2002')
-    sents_mocked.assert_called_with('esp.testb')
+    corpus_mocked.conll2002.iob_sents.assert_called_with('esp.testb')
