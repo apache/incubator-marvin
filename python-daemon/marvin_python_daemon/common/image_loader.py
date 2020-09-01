7cbaf6d5c7469e25c60390e0d347d738e1460f04
sette <brunosette@protonmail.com>
2020-09-01 14:17:30 -0300

    Reader from dataframe added

--- python-daemon/marvin_python_daemon/common/image_loader.py
+++ python-daemon/marvin_python_daemon/common/image_loader.py
@@ -57,12 +57,7 @@ class ImageLoader(MarvinData):
         img = imresize(img, (shape[0], shape[1], 3))
         return img
 
-    @classmethod
-    # get image tag
-    def img_label(cls,path):
-        return str(str(path.split('/')[-1]))
-
-
+    
     @classmethod
     def img_class(cls,path):
         return str(path.split('/')[-2])
@@ -78,12 +73,22 @@ class ImageLoader(MarvinData):
         for p in tqdm(paths, ascii=True, ncols=85, desc=text):
             img = imageio.imread(p)
             img = cls.img_reshape(img,shape)
-            somdata_dicte_dict['image'].append(img)
-            data_dict['label'].append(cls.img_label(p))
+            data_dict['image'].append(img)
             if 'train' in paths[0]:
                 data_dict['class'].append(cls.img_class(p))
 
         return data_dict
+
+    @classmethod
+    def fill_df(cls,paths, train_dict,shape):
+        train_dict['image'] = []
+        text = 'Start fill train_dict'
+        for p in tqdm(paths, ascii=True, ncols=85, desc=text):
+            img = imageio.imread(p)
+            img = cls.img_reshape(img,shape)
+            train_dict['image'].append(img)
+
+        return train_dict
 
 
     @classmethod
@@ -101,7 +106,7 @@ class ImageLoader(MarvinData):
         return(img)
 
     @classmethod
-    def reader(cls,relpath,shape):
+    def reader_from_directory(cls,relpath,shape):
         """
         Load data from the following sources in order of priority:
         1. Filesystem
@@ -133,15 +138,42 @@ class ImageLoader(MarvinData):
                         test_path.append(path)
         train_dict = {
             'image': [],
-            'label': [],
             'class': []
         }
         test_dict = {
-            'image': [],
-            'label': []
+            'image': []
         }
 
         train_dict = cls.fill_dict(train_path, train_dict,shape)
         test_dict = cls.fill_dict(test_path, test_dict,shape)
         return train_dict, test_dict
+
+    @classmethod
+    def reader_from_dataframe(cls,dataframe,directory,shape):
+        """
+        Load data from the following sources in order of priority:
+        1. Filesystem
+        :param dataframe:  Pandas dataframe containing the filepaths relative to 
+        directory (or absolute paths if directory is None) of the images in a string 
+        column. it must include the y_col column with the class/es of each image.
+        :param directory: string, path to the directory to read images from. 
+        If None, data in x_col column should be absolute paths.
+        :param shape: shape of images
+        :return: dict - data content
+        """
+        path = os.path.join(cls.get_data_path(), directory)
+
+        columns = dataframe.columns
+
+        dataframe = dataframe.apply(lambda x: path+os.sep+x if x.name == columns[0] else x)
+
+        dataframe = {
+            'image': dataframe[columns[0]],
+            'class': dataframe[columns[1]]
+        }
+
+        dataframe = cls.fill_df(dataframe.get("image"), dataframe,shape)
+        
+        return dataframe
+
 
